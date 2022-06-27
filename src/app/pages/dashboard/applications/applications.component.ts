7b01cbe629f9de6bdf20d253be5edb7743270ed0
Henry Bugajski <hjbugajski@gmail.com>
2022-06-26 20:41:52 -0400

    feat: add board column layout for applications component

--- src/app/pages/dashboard/applications/applications.component.ts
+++ src/app/pages/dashboard/applications/applications.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+import { TITLE_SUFFIX } from '~constants/title.constant';
 
 @Component({
   selector: 'at-applications',
   templateUrl: './applications.component.html',
-  styleUrls: ['./applications.component.scss']
+  styleUrls: ['./applications.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ApplicationsComponent {}
+export class ApplicationsComponent {
+  public columns = ['Todo', 'In progress', 'Submitted', 'Interview'];
+  public items = Array.from({ length: 50 }).map((_, i) => i);
+
+  constructor(private titleService: Title) {
+    this.titleService.setTitle('Applications' + TITLE_SUFFIX);
+  }
+}
--- src/app/pages/dashboard/applications/applications.module.ts
+++ src/app/pages/dashboard/applications/applications.module.ts
@@ -1,11 +1,26 @@
+import { ScrollingModule } from '@angular/cdk/scrolling';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterModule } from '@angular/router';
 
 import { ApplicationsComponent } from './applications.component';
 
 @NgModule({
   declarations: [ApplicationsComponent],
-  imports: [CommonModule, RouterModule.forChild([{ path: '', component: ApplicationsComponent }])]
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDividerModule,
+    MatIconModule,
+    MatTooltipModule,
+    RouterModule.forChild([{ path: '', component: ApplicationsComponent }]),
+    ScrollingModule
+  ]
 })
 export class ApplicationsModule {}
