3df6554403df1e18779be2b0015c30a575c594d6
Henry Bugajski <hjbugajski@gmail.com>
2022-07-17 17:46:29 -0400

    fix: remove sidenav and toolbar components, move functionality into dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,7 +2,16 @@ import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatDrawerMode, MatSidenav } from '@angular/material/sidenav';
 import { Subscription } from 'rxjs';
 
+import { Icons } from '~enums/icons.enum';
+import { Paths } from '~enums/paths.enum';
+import { AuthService } from '~services/auth/auth.service';
 import { SidenavService } from '~services/sidenav/sidenav.service';
+
+interface SidenavItem {
+  icon: string;
+  viewValue: string;
+  route: string;
+}
 
 @Component({
   selector: 'at-dashboard',
@@ -14,13 +23,31 @@ export class DashboardComponent implements OnInit, OnDestroy {
 
   public mode: MatDrawerMode;
   public opened: boolean;
+  public sidenavItems: SidenavItem[];
 
   private subscriptions: Subscription;
 
-  constructor(public sidenavService: SidenavService) {
+  constructor(private authService: AuthService, public sidenavService: SidenavService) {
     this.mode = 'side';
     this.opened = true;
     this.subscriptions = new Subscription();
+    this.sidenavItems = [
+      {
+        icon: Icons.CarbonApplication,
+        viewValue: 'Applications',
+        route: Paths.Applications
+      },
+      // {
+      //   icon: Icons.CarbonChartMultitype,
+      //   viewValue: 'Analytics',
+      //   route: Paths.Analytics
+      // },
+      {
+        icon: Icons.CarbonTemplate,
+        viewValue: 'Job Boards',
+        route: Paths.JobBoards
+      }
+    ];
   }
 
   ngOnDestroy(): void {
@@ -39,5 +66,19 @@ export class DashboardComponent implements OnInit, OnDestroy {
       })
     );
     this.sidenavService.sidenav = this._sidenav!;
+  }
+
+  public async signOut(): Promise<void> {
+    await this.authService.signOut();
+  }
+
+  public get paths(): typeof Paths {
+    return Paths;
+  }
+
+  public async toggleSidenav(): Promise<void> {
+    if (this.sidenavService.showMenuButton.getValue()) {
+      await this.sidenavService.sidenav.toggle();
+    }
   }
 }
--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -1,15 +1,37 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { RouterModule } from '@angular/router';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 
-import { SidenavModule } from '~components/sidenav/sidenav.module';
-import { ToolbarModule } from '~components/toolbar/toolbar.module';
+import { BoardSelectorModule } from '~components/board-selector/board-selector.module';
+import { ThemePickerModule } from '~components/theme-picker/theme-picker.module';
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [CommonModule, DashboardRoutingModule, MatSidenavModule, SidenavModule, ToolbarModule]
+  imports: [
+    BoardSelectorModule,
+    CommonModule,
+    DashboardRoutingModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatListModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    RouterModule,
+    ThemePickerModule
+  ]
 })
 export class DashboardModule {}
