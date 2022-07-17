106838630fd00f9ad374be5f997c5a70cb793dcd
Henry Bugajski <hjbugajski@gmail.com>
2022-07-17 18:59:27 -0400

    fix: update job board component

--- src/app/pages/dashboard/job-boards/job-boards.module.ts
+++ src/app/pages/dashboard/job-boards/job-boards.module.ts
@@ -2,10 +2,6 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSortModule } from '@angular/material/sort';
-import { MatTableModule } from '@angular/material/table';
-import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterModule } from '@angular/router';
 
 import { JobBoardsComponent } from './job-boards.component';
@@ -16,10 +12,6 @@ import { JobBoardsComponent } from './job-boards.component';
     CommonModule,
     MatButtonModule,
     MatCardModule,
-    MatIconModule,
-    MatSortModule,
-    MatTableModule,
-    MatTooltipModule,
     RouterModule.forChild([{ path: '', component: JobBoardsComponent }])
   ]
 })
