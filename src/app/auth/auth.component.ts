80d58db9e7c813ff748b256c8fe307594bf2480f
Henry Bugajski <henry.bug.dev@gmail.com>
2022-11-08 18:19:08 -0500

    fix: small style updates

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+
+import { ThemeService } from '~services/theme/theme.service';
 
 @Component({
   selector: 'at-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss']
 })
-export class AuthComponent {}
+export class AuthComponent implements OnDestroy {
+  constructor(private themeService: ThemeService) {
+    this.themeService.addBackgroundClass('at-alt-background');
+  }
+
+  ngOnDestroy(): void {
+    this.themeService.removeBackgroundClass('at-alt-background');
+  }
+}
